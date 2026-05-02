27c2276ed60892a3a030ad036f0411a489ffa059
moskvichev <moskvichev@keeneye.pro>
2026-05-02 21:29:30 +0400

    init commit

---
+++ backend/src/main/java/com/doppelganger/network_digital_twin/NetworkDigitalTwinApplication.java
@@ -0,0 +1,13 @@
+package com.doppelganger.network_digital_twin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkDigitalTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkDigitalTwinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/doppelganger/network_digital_twin/NetworkDigitalTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doppelganger.network_digital_twin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkDigitalTwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
